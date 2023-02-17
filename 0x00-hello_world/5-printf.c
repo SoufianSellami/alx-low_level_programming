f9ca24605ca8465bf974ac14e7639fcb800641dd
Soufian Sellami <sellamisoufian0@gmail.com>
2023-02-17 15:50:44 -0800

    function Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Print 'with proper grammar, but the outcome is a piece of art,'
+ * return 0
+ */
+int main(void)
+{
+	Printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
