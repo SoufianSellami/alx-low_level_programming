26c8446e4569faa999990f8e51ee6c58ecc57f64
Soufian Sellami <sellamisoufian0@gmail.com>
2023-02-17 15:54:42 -0800

    function printfV1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - Print 'with proper grammar, but the outcome is a piece of art,'
+ * main - Print "with proper grammar, but the outcome is a piece of art,"
  * return 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
