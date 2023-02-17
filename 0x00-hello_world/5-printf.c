9dac8181b9edad475818f99e16cb81e0f8d123e0
Soufian Sellami <sellamisoufian0@gmail.com>
2023-02-17 15:59:30 -0800

    function  printfV3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - Print "with proper grammar, but the outcome is a piece of art,"
- * Return 0
+/**
+ *main - Prints "with proper grammar, but the outcome is a piece of art"
+ * Return: Always 0.
  */
 int main(void)
 {
