ba7d0af301a48ccde474b93bc5a209e99d5cfc08
Soufian Sellami <sellamisoufian0@gmail.com>
2023-02-17 17:15:56 -0800

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,8 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/* main - This program will assign a random number to the variable n each time it is executed*/
+/* Return 0 */
 int main(void)
 {
 		int n;
