62ff362f62dfaa4da10aa828c67bc5c5d042f60c
Soufian Sellami <sellamisoufian0@gmail.com>
2023-02-17 18:49:07 -0800

    advanced doneV1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,14 +22,14 @@ int main(void)
 
 					if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 						continue;
-					
-					putchar(',');
-					putchar(' ');
+				putchar(',');
+				putchar(' ');
+
 				}
 			}
 		}
 		
 		putchar('\n');
-		
+
 		return (0);
 }
